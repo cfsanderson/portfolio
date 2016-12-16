617ba1fa59f9a90e041fc764a68bfe69d991e0f9
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-12-16 09:34:50 -0500

    getting started

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,56 @@
 import React, { Component } from 'react'
+// import picture from './path-to-pic.jpg'
 
 class App extends Component {
 
   render () {
-    return <div>
-      <h1>Hello, World!</h1>
-    </div>
+    return (
+      <div>
+        <h1>Caleb Frost Sanderson</h1>
+        <img src={picture} />
+        <h4>I'm a Front-End Engineering student at The Iron Yard in St. Petersburg, FL.</h4>
+      </div>
+      //     </section>
+      //     <section className='game-thumbs'>
+      //       <ul className='thumbs'>
+      //         <li style="background-image: url('./images/01one.png');" />
+      //         <li style="background-image: url('./images/02two.png');" />
+      //         <li style="background-image: url('./images/03three.png');" />
+      //         <li style="background-image: url('./images/04four.png');" />
+      //       </ul>
+      //     </section>
+      //   </header>
+      //   <main>
+      //     <article>
+      //       <header style="background-image: url('./images/steelmills3.JPG');" />
+      //       <div>
+      //         <h3>I’m Headed to The Iron Yard<time>November 14, 2016</time></h3>
+      //         <p>That title would have meant something very different 50 years ago. It brings up images of grease smudged faces queued up to punch a time clock. Today it means that I’m starting an immersive program or “bootcamp” for software development with an emphasis in Front-end Engineering (HTML, CSS, Javascript, and React) at a school called The Iron Yard in Tampa, FL (downtown St. Petersburg actually).</p>
+      //         <a className='keep-reading' href='https://medium.com/@cfsanderson/script-src-https-gist-github-com-cfsanderson-db861b186bfbb72e98f467ed20415e40-js-script-9329d14bfd58#.cc6nodbfl' target='_blank'>KEEP READING</a>
+      //       </div>
+      //     </article>
+      //     <article>
+      //       <header style="background-image: url('./images/fizzbuzzed-js.jpg');" />
+      //       <div>
+      //         <h3>FizzBuzzed: Why Javascript? <time>November 15th, 2016</time></h3>
+      //         <p>Today is the first day of week 2 in my 12 week cohort at The Iron Yard. Last week was spent learning HTML and CSS which I’d had enough exposure to that I could recognize some of the syntax, but writing functional code was a struggle. I knew the pace of the course would be fast but I wasn’t expecting a panic attack on day 2. OK, it wasn’t quite that bad, but almost… and only DAY 2?! After some encouragement from my much wiser, patient, and kind wife, I put my head down and got to work. By the end of the week I had reproduced a functioning website from a static mockup image (style is not mine so no design critiques please — purely functional), personalized it with some of my info, and even added some responsive design elements. Given how I felt just a few days before, I felt a huge amount of relief and even some growing confidence handing in this assignment.</p>
+      //         <a className='keep-reading' href='https://medium.com/@cfsanderson/fizzbuzzed-why-javascript-43ad273aa989#.aeag0p9rj' target='_blank'>KEEP READING</a>
+      //       </div>
+      //     </article>
+      //     <footer>
+      //       <h2>Stay connected with me.</h2>
+      //       <ul>
+      //         <li><a href='https://twitter.com/cfsanderson' target='_blank'><i className='fa fa-twitter' aria-hidden='true' /></a></li>
+      //         <li><a href='https://www.facebook.com/caleb.f.sanderson' target='_blank'><i className='fa fa-facebook' aria-hidden='true' /></a></li>
+      //         <li><a href='https://www.instagram.com/cfsanderson/' target='_blank'><i className='fa fa-instagram' aria-hidden='true' /></a></li>
+      //         <li><a href='https://www.linkedin.com/in/calebsanderson' target='_blank'><i className='fa fa-linkedin' aria-hidden='true' /></a></li>
+      //         <li><a href='https://github.com/cfsanderson' target='_blank'><i className='fa fa-github' aria-hidden='true' /></a></li>
+      //       </ul>
+      //       <h5>Copyright &copy; 2016 Caleb Frost Sanderson. Built with <i className='fa fa-heart' aria-hidden='true' /> at The Iron Yard</h5>
+      //     </footer>
+      //   </main>
+      // </div>
+    )
   }
 }
 
---
+++ src/components/BlogFeed.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class BlogFeed extends Component {
+
+  render () {
+    return <div>
+      <h1>Hello, World!</h1>
+    </div>
+  }
+}
+
+export default BlogFeed
