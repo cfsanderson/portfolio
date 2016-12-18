c102651c7af982adada3fb12a8fa7f53b8080931
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-12-18 13:52:57 -0500

    title and subtitle OK, working on 'view resume' button

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,9 +8,16 @@ class Header extends Component {
       <div>
         <header className='top'>
           <h1>{this.props.name}</h1>
-          <h3>Front End Developer</h3>
-          <button>Download my resume here.</button>
-          {/* with modal link to my resume */}
+          <h2>Front End Developer</h2>
+          <button><a className='resumeLink' href=''>VIEW RÉSUMÉ</a></button>
+          <div className='resumeoverlay'>
+            <div className='dialog'>
+              <h3>!!!</h3>
+              <p>Below is my current résumé Insert PDF here.</p>
+              <button>X close window X</button>
+            </div>
+          </div>
+          {/* with modal link to my resume https://www.linkedin.com/in/calebsanderson */}
           {/* <div className='bio'>
             <h4>{this.props.bio}</h4>
             <img className='avatar_url' src={this.props.avatar_url} />
