fd558bb5e28b9c506a291081abf527b662a2d901
Caleb Frost Sanderson <calebsanderson@gmail.com>
2017-01-07 22:10:09 -0500

    Fixed Google Drive resume link.

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ class Header extends Component {
         <header className='top'>
           <h1>{this.props.name}</h1>
           <h2>Front End Developer</h2>
-          <button><a className='resumeLink' href=''>VIEW RÉSUMÉ</a></button>
+          <button><a className='resumeLink' href='https://drive.google.com/file/d/0BwvRMwAwXIPELXQtQ2F0Y2Y5bG8/view?usp=sharing'>VIEW RÉSUMÉ</a></button>
           {/* with modal link to my resume https://www.linkedin.com/in/calebsanderson */}
           {/* <div className='bio'>
             <h4>{this.props.bio}</h4>
