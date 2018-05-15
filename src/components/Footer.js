2c05a8aebf17cdbde470a8f500f92fcf8f4841c2
cfsanderson <calebsanderson@gmail.com>
2018-05-15 19:54:32 -0400

    Removes FB in social footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,7 +11,6 @@ class Footer extends Component {
           <h2>Stay connected</h2>
           <ul className='socials'>
             <li><a href='https://twitter.com/cfsanderson' target='_blank'><i className='fa fa-twitter' aria-hidden='true' /></a></li>
-            <li><a href='https://www.facebook.com/caleb.f.sanderson' target='_blank'><i className='fa fa-facebook' aria-hidden='true' /></a></li>
             <li><a href='https://www.instagram.com/cfsanderson/' target='_blank'><i className='fa fa-instagram' aria-hidden='true' /></a></li>
             <li><a href='https://www.linkedin.com/in/calebsanderson' target='_blank'><i className='fa fa-linkedin' aria-hidden='true' /></a></li>
             <li><a href='https://github.com/cfsanderson' target='_blank'><i className='fa fa-github' aria-hidden='true' /></a></li>
