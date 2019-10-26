10604f4a052f185766eebacf79e37e414da374bf
cfsanderson <calebsanderson@gmail.com>
2019-10-25 23:30:11 -0400

    updating

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import styles from '../styles/screen.scss'
+// import styles from '../styles/screen.scss'
 
 class Footer extends Component {
 
@@ -11,7 +11,6 @@ class Footer extends Component {
           <h2>Stay connected</h2>
           <ul className='socials'>
             <li><a href='https://twitter.com/cfsanderson' target='_blank'><i className='fa fa-twitter' aria-hidden='true' /></a></li>
-            <li><a href='https://www.instagram.com/cfsanderson/' target='_blank'><i className='fa fa-instagram' aria-hidden='true' /></a></li>
             <li><a href='https://www.linkedin.com/in/calebsanderson' target='_blank'><i className='fa fa-linkedin' aria-hidden='true' /></a></li>
             <li><a href='https://github.com/cfsanderson' target='_blank'><i className='fa fa-github' aria-hidden='true' /></a></li>
           </ul>
